6b04e81f8b6bcf3b552fe45cc43f0a09ce924c7d
AdrianUrsuTecnoCampus <AdrianUrsuTecnoCampus@users.noreply.github.com>
2024-10-23 09:52:02 +0200

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-
 public class Main {
-    public void main (String[] args) {
-        System.out.println("Hola mundo!");
+    public static void main(String[] args) {
+        System.out.println("A");
     }
 }
