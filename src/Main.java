9b1dad602bfdd7f57276ab4835171e3ad2905938
Davidgaudioso15 <162585189+Davidgaudioso15@users.noreply.github.com>
2024-10-30 09:45:57 +0100

    ddd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //System.out.println("Adrian".compareTo("David"));
 
         BinaryTree a = new BinaryTree();
 
+        a.addNode(new Person(2, "Barcelona", "David"),"L");
+        a.addNode(new Person(3, "Barcelona", "Rosa"),"R");
+        a.addNode(new Person(4, "Barcelona", "Pau"),"LL");
+        a.addNode(new Person(1, "Barcelona", "Maria"),"RL");
+        a.addNode(new Person(2, "Barcelona", "Laura"),"RL");
+        a.addNode(new Person(3, "Barcelona", "Marta"),"RR");
+
+        a.displayTree();
 
     }
 }
