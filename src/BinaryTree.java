057d2c9cac463b29e621d62716f0be93ae446fd9
Davidgaudioso15 <162585189+Davidgaudioso15@users.noreply.github.com>
2024-10-30 09:18:19 +0100

    ccc

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -29,7 +29,7 @@ public class BinaryTree {
             }
 
             else if (left == null) {
-                linea += ";";
+                linea += " ;";
             }
             else if (right == null) {
                 linea += " ;";
